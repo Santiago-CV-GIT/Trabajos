20f84ac168701c8ffdbe271996e09a6a5ee0fae3
scv-git <vegacifuentessantiago@gmail.com>
2025-04-09 12:01:09 -0500

    act

--- batallon/src/main/java/co/edu/uniquindio/edu/co/model/Apoyo.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.edu.co.model;
-
-public class Apoyo {
-}
--- batallon/src/main/java/co/edu/uniquindio/edu/co/model/Vehiculo.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.edu.co.model;
-
-public abstract class Vehiculo {
-    public String id;
-    public String modelo;
-    fecha DA
-}
