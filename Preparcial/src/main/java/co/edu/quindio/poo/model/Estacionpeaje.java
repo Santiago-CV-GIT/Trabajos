b884d0adf78d140d38a4e3f8cef474c68185984a
scv-git <vegacifuentessantiago@gmail.com>
2025-05-24 19:56:16 -0500

    act

---
+++ Preparcial/src/main/java/co/edu/quindio/poo/model/Estacionpeaje.java
@@ -0,0 +1,9 @@
+package co.edu.quindio.poo.model;
+
+public class Estacionpeaje {
+    private String nombre;
+    private String departamento;
+    private float peajerecolectado;
+    private ArrayList<Vehiculo> listavehiculos;
+    private ArrayList<Personas> listapersonas;
+}
